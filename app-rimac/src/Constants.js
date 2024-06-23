65bbc948a6836e7b048fcebaeccd7430f59f5962
Anitazc1314 <anamaria.zarate@rainfocus.com>
2024-06-23 07:44:46 -0500

    Added question sets and css

---
+++ app-rimac/src/Constants.js
@@ -0,0 +1,63 @@
+const options = ["Nunca", "Varios días", "Más de la mitad de los días", "Casi todos los días"];
+
+export const anxietyQuestions = [
+  {
+    id: 1,
+    question: "Sentirse nervioso/a, intranquilo/a o con los nervios de punta",
+    options
+  },
+  {
+    id: 2,
+    question: "No poder dejar de preocuparse o no poder controlar la preocupación",
+    options
+  },
+  {
+    id: 3,
+    question: "Preocuparse demasiado por diferentes cosas",
+    options
+  },
+  {
+    id: 4,
+    question: "Dificultad para relajarse",
+    options
+  },
+  {
+    id: 5,
+    question: "Estar tan inquieto/a que es difícil permanecer sentado/a tranquilamente",
+    options
+  },
+  {
+    id: 6,
+    question: "Molestarse o ponerse irritable fácilmente",
+    options
+  },
+  {
+    id: 7,
+    question: "Sentir miedo como si algo terrible pudiera pasar",
+    options
+  }
+];
+
+export const infoCardsData = [
+    {
+      id: 1,
+      title: 'Vida Ahorro con Devolución',
+      description: 'Te devolvemos hasta 200% de tus aportes',
+      buttonText: '+ Información',
+      imgsrc: "https://www.rimac.com/ventadigital/devolucion/assets/rebrand/home/hero/vcd-hero-mob.png"
+    },
+    {
+      id: 2,
+      title: 'Seguro Universitario',
+      description: 'Asegura el futuro de tus hijos con un fondo universitario',
+      buttonText: '+ Información',
+      imgsrc:"https://imagescdn.rimac.com/blt20549486134201e8/64270a9bb663cf12b9410436/mujer-viendo-celular-tranquilo-casa-usuario.png?auto=webp&quality=85&width=226"
+    },
+    {
+      id: 3,
+      title: 'Seguro de Salud',
+      description: 'Te atendemos en más de 300 clínicas',
+      buttonText: '+ Información',
+      imgsrc:"https://imagescdn.rimac.com/blt2fe8aa8fdd9b60b7/65f864b573d0bcaa48f3c3f4/card-eps-mobile.png?auto=webp&quality=85&width=600"
+    }
+  ];
--- app-rimac/src/components/ContactForm.js
+++ app-rimac/src/components/ContactForm.js
@@ -1,11 +1,14 @@
+// src/components/ContactForm.js
 import React, { useState } from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
+import Textbox from './textbox/Textbox';
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
     name: '',
+    celular: '',
     email: '',
-    message: ''
+    acceptPolicy: false // Estado para aceptar la política de comunicaciones (checkbox)
   });
   const [captchaValue, setCaptchaValue] = useState('');
 
@@ -13,35 +16,77 @@ const ContactForm = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const handleAcceptPolicyChange = () => {
+    setFormData({
+      ...formData,
+      acceptPolicy: !formData.acceptPolicy
+    });
+  };
+
   const handleCaptchaChange = (value) => {
     setCaptchaValue(value);
-    console.log(value, 'value')
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // validate
+
     if (!captchaValue) {
-      alert('Please, try the reCAPTCHA.');
+      alert('Please, complete the reCAPTCHA.');
       return;
     }
+
     console.log('Form Data:', formData);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="name" placeholder="Name" value={formData.name} onChange={handleChange} required />
-      <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required />
-      <textarea name="message" placeholder="Message" value={formData.message} onChange={handleChange} required />
-
-      <ReCAPTCHA
-        sitekey="6LcgGf8pAAAAAN5E9I-545Eo4LKAcYPdVqOBJcEf"
-        onChange={handleCaptchaChange}
-      />
-
-      <button type="submit">Send</button>
-    </form>
+    <><div className="container">
+      <h1>Mide tu nivel de ansiedad</h1>
+      <div className="form-wrapper">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Nombres y Apellidos</label>
+            <Textbox
+              id="name"
+              placeholder="Name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="celular">Celular</label>
+            <Textbox
+              id="celular"
+              placeholder="Celular"
+              name="celular"
+              value={formData.celular}
+              onChange={handleChange} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <Textbox
+              id="email"
+              placeholder="Email"
+              name="email"
+              value={formData.email}
+              s onChange={handleChange} />
+          </div>
+        </form>
+      </div>
+    </div><div className="policy-container">
+        <label>Al continuar acepto la Politica de Privacidad</label>
+        <label className="checkbox-label">
+          <input
+            type="checkbox"
+            checked={formData.acceptPolicy}
+            onChange={handleAcceptPolicyChange} />
+          Acepto la política de comunicaciones
+        </label>
+      </div><div className="form-catpcha">
+        <ReCAPTCHA
+          sitekey="your-recaptcha-site-key"
+          onChange={handleCaptchaChange} />
+      </div></>
   );
-}
+};
 
 export default ContactForm;
